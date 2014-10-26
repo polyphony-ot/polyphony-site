dfe414c229a07a041fb180b629bc32467887470c
Greg Curtis <greg.r.curtis@gmail.com>
2014-10-25 17:09:47 -0700

    Update deploy script to change port
    This is hacky, but the deploy script will use sed to change the port for
    the OT server. This way, the staging and production site can be run on
    the same machine.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -4,4 +4,9 @@ addr="staging.polyphony-ot.com"
 dir="/home/polyphony/staging"
 
 rsync -rtv --exclude "*.git" --exclude "deploy-staging.sh" ./ "$addr:$dir"
-ssh "$addr" "chmod -R u+rwX,go+rwX,o-w $dir && chown -R :polyphony $dir"
+ssh "$addr" << EOF
+	chmod -R u+rwX,go+rwX,o-w $dir
+	chown -R :polyphony $dir
+	sed -i 's/:51015/:51016/g' "$dir/scripts/polyphony-demo.js"
+	sed -i 's/port: 51015/port: 51016/g' "$dir/server/lib/socket-server.js"
+EOF
